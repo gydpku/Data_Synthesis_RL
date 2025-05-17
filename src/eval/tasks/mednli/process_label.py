f7c0044cdeaf0dff65aae8315ac09618557518bd
gydpku <58905134+gydpku@users.noreply.github.com>
2025-05-17 22:00:50 +0800

    Update process_label.py

--- src/eval/tasks/mednli/process_label.py
+++ src/eval/tasks/mednli/process_label.py
@@ -1,16 +1,4 @@
 import re
 def process_label(solution_str):
-    answer_pattern = r'<answer>(.*?)</answer>'
-    match = re.finditer(answer_pattern, solution_str)
-    matches = list(match)
-    if matches:
-        final_answer = matches[-1].group(1).strip()
-    else:
-        final_answer = None
-#    if final_answer is not None:
- #       try:
- #           int_final_answer = int(final_answer)
- #       except ValueError:
- #           final_answer = None
-    return final_answer
-
+    
+    return solution_str
